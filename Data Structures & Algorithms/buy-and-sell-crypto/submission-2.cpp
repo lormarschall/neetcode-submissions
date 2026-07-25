3f0d22d66f9aa9cfe169bb53585530c6cdb5a638
lormarschall <lor.marschall@gmail.com>
2026-07-25 17:53:17 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int left = 0;
+      int right = 0;
+      int max_p = 0; 
+
+      while(right < prices.size()){
+        max_p = max(max_p, prices[right] - prices[left]);
+        if(prices[left] <= prices[right]){
+            right++;
+        }
+        else{
+            left++;
+        }
+      }
+
+      return max_p;
+    }
+};
